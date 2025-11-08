1c1eb61563ba42a02e1c8e9293dc119c9b3bddc2
codesofC <inelusjudelin01@gmail.com>
2025-11-07 22:32:05 -0300

    jinelus : #16 : feat(ai): add AI provider and integrate with trigger example

--- apps/front-end/src/components/example/index.tsx
+++ apps/front-end/src/components/example/index.tsx
@@ -1,5 +1,6 @@
 import { useMutation } from '@tanstack/react-query'
 import { Button } from '../ui/button'
+import { aiProvider } from './ai-provider'
 import { triggerServerFn } from './fn'
 
 export function TriggerExample() {
@@ -7,19 +8,40 @@ export function TriggerExample() {
     mutationFn: () => triggerServerFn({ data: { name: 'hello-world' } }),
   })
 
-  return (
-    <div className="mt-6 space-y-4">
-      <Button onClick={() => mutate()} disabled={isPending}>
-        Run Trigger Task
-        {isError && <span>Error occurred</span>}
-      </Button>
+  const executeAiProviderTest = useMutation({
+    mutationFn: () => aiProvider({ data: { prompt: 'What is love?' } }),
+  })
 
-      {isPending && <p>Triggering task...</p>}
-      {isSuccess && (
-        <div>
-          <p>Task triggered successfully!</p>
-        </div>
-      )}
+  return (
+    <div className="flex items-center gap-14">
+      <div className="mt-6 space-y-4">
+        <Button onClick={() => mutate()} disabled={isPending}>
+          Run Trigger Task
+          {isError && <span>Error occurred</span>}
+        </Button>
+
+        {isPending && <p>Triggering task...</p>}
+        {isSuccess && (
+          <div>
+            <p>Task triggered successfully!</p>
+          </div>
+        )}
+      </div>
+      <div className="mt-6 space-y-4">
+        <Button
+          onClick={() => executeAiProviderTest.mutate()}
+          disabled={executeAiProviderTest.isPending}
+        >
+          Run AI Provider Test
+        </Button>
+        {executeAiProviderTest.isPending && <p>Generating text...</p>}
+        {executeAiProviderTest.isSuccess && (
+          <div>
+            <p>Generated Text:</p>
+            <pre>{executeAiProviderTest.data.id}</pre>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
