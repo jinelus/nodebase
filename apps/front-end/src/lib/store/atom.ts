b1b289e79fc2f7dc6d67a0615b9946caaad600cc
codesofC <inelusjudelin01@gmail.com>
2025-12-14 12:41:48 -0300

    f#56 : eat(atom): add workflowExecutionAtom for managing execution state

--- apps/front-end/src/lib/store/atom.ts
+++ apps/front-end/src/lib/store/atom.ts
@@ -2,3 +2,8 @@ import type { ReactFlowInstance } from '@xyflow/react'
 import { atom } from 'jotai'
 
 export const editorAtom = atom<ReactFlowInstance | null>(null)
+
+export const workflowExecutionAtom = atom<{
+  runId: string | null
+  publicAuthToken: string | null
+} | null>(null)
