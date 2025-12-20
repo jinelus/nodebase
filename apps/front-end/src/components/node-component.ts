0e1e26aa9ea7e102d7ce03a28ec2bcea65b9658c
codesofC <inelusjudelin01@gmail.com>
2025-12-20 19:43:37 -0300

    #60 : feat(node): add Google Form trigger node and update selector options

--- apps/front-end/src/components/node-component.ts
+++ apps/front-end/src/components/node-component.ts
@@ -1,5 +1,6 @@
 import type { NodeTypes } from '@xyflow/react'
 import { HttpRequestNode } from '@/features/executions/http-request/node'
+import { GoogleFormTriggerNode } from '@/features/triggers/google-form/node'
 import { ManualTriggerNode } from '@/features/triggers/manual-trigger/node'
 import { InitialNode } from './react-flow/initial-node'
 
@@ -7,13 +8,19 @@ export const NODETYPE = {
   INITIAL: 'INITIAL',
   MANUAL_TRIGGER: 'MANUAL_TRIGGER',
   HTTP_REQUEST: 'HTTP_REQUEST',
+  GOOGLE_FORM_TRIGGER: 'GOOGLE_FORM_TRIGGER',
 } as const
 
 export type NodeType = (typeof NODETYPE)[keyof typeof NODETYPE]
 
 export const nodeComponents = {
   [NODETYPE.INITIAL]: InitialNode,
+
+  // Trigger Nodes
   [NODETYPE.MANUAL_TRIGGER]: ManualTriggerNode,
+  [NODETYPE.GOOGLE_FORM_TRIGGER]: GoogleFormTriggerNode,
+
+  // Execution Nodes
   [NODETYPE.HTTP_REQUEST]: HttpRequestNode,
 } as const satisfies NodeTypes
 
