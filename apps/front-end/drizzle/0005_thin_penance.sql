beda31844d606ddd07c013d9bc1de674cf94f6a8
codesofC <inelusjudelin01@gmail.com>
2025-12-24 23:05:13 -0300

    #66 : feat(database): add new types 'GEMINI', 'OPENAI', 'ANTHROPIC', 'GROK', and 'DEEPSEEK' to enums and update journal

---
+++ apps/front-end/drizzle/0005_thin_penance.sql
@@ -0,0 +1,4 @@
+ALTER TYPE "public"."types" ADD VALUE 'OPENAI';--> statement-breakpoint
+ALTER TYPE "public"."types" ADD VALUE 'ANTHROPIC';--> statement-breakpoint
+ALTER TYPE "public"."types" ADD VALUE 'GROK';--> statement-breakpoint
+ALTER TYPE "public"."types" ADD VALUE 'DEEPSEEK';
