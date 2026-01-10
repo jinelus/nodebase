6a79cfb9e829fd5b1fdc1252cb9cfd9def8deb45
codesofC <inelusjudelin01@gmail.com>
2026-01-10 20:50:54 -0300

    #85 : fix(auth): update baseURL to use BETTER_AUTH_URL environment variable

--- apps/front-end/src/lib/auth-client.ts
+++ apps/front-end/src/lib/auth-client.ts
@@ -3,6 +3,6 @@ import { createAuthClient } from 'better-auth/react'
 import { env } from '@/utils/env'
 
 export const authClient = createAuthClient({
-  baseURL: env.VITE_BETTER_AUTH_URL ?? 'http://localhost:3000',
+  baseURL: env.BETTER_AUTH_URL,
   plugins: [polarClient()],
 })
